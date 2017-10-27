9989254f75cb07a03f6af7e424f0d2eb68e4bee5
SOORAJ <vsooraj@gmail.com>
2017-10-27 22:33:34 +0530

    New full template project added and beautify the pages

---
+++ TranslatorWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TranslatorWeb.MvcApplication" Language="C#" %>
---
+++ TranslatorWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TranslatorWeb.Startup))]
+namespace TranslatorWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
