781c85d02c2cb9172f302f927b3a4d2de06df3be
SOORAJ <vsooraj@gmail.com>
2017-10-29 19:25:49 +0530

    Platform CRUD

---
+++ TranslatorWeb/Controllers/PlatformsController.cs
@@ -0,0 +1,123 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using TranslatorWeb.Models;
+
+namespace TranslatorWeb.Controllers
+{
+    public class PlatformsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Platforms
+        public ActionResult Index()
+        {
+            return View(db.Platforms.ToList());
+        }
+
+        // GET: Platforms/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Platform platform = db.Platforms.Find(id);
+            if (platform == null)
+            {
+                return HttpNotFound();
+            }
+            return View(platform);
+        }
+
+        // GET: Platforms/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Platforms/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,IsActive,ImageUrl")] Platform platform)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Platforms.Add(platform);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(platform);
+        }
+
+        // GET: Platforms/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Platform platform = db.Platforms.Find(id);
+            if (platform == null)
+            {
+                return HttpNotFound();
+            }
+            return View(platform);
+        }
+
+        // POST: Platforms/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,IsActive,ImageUrl")] Platform platform)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(platform).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(platform);
+        }
+
+        // GET: Platforms/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Platform platform = db.Platforms.Find(id);
+            if (platform == null)
+            {
+                return HttpNotFound();
+            }
+            return View(platform);
+        }
+
+        // POST: Platforms/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Platform platform = db.Platforms.Find(id);
+            db.Platforms.Remove(platform);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
